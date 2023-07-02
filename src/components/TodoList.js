8f4af1d5ea3d08212c1b199c38f1f3179c82873c
Ignacio Balsells <deusnayo@gmail.com>
2023-07-01 19:10:56 -0600

    Starting backen using backend endpoints

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,9 +6,10 @@ import { useEffect } from "react";
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
-  useEffect(async () => {
+  // Consulta la base de datos para traer la lista de ToDos
+  const getTodo = async () => {
     try {
-      const response = await fetch("https://todo-api-h8ov.onrender.com/api", {
+      const response = await fetch("http://localhost:4000/api", {
       method: "GET"
     })
     const res = await response.json()
@@ -20,15 +21,18 @@ function TodoList() {
     } catch (error) {
       console.error(`Couldn't retrieve ToDos: ${error.message}`)
     }
-  });
+  }
+  
+  useEffect(async () => {
+    getTodo()
+  }, []);
 
+  // Agrega nuevo elemento de ToDo a la base de datos
   const addTodo = async (todo) => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
     }
-
     const { text, description } = todo
-    console.log("todo:",text,description)
     try {
       const request = await fetch("https://todo-api-h8ov.onrender.com/api", {
       method: "POST",
@@ -36,17 +40,14 @@ function TodoList() {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({title: text, description: description})
+      body: JSON.stringify({title: text, description: description.length>0 ? description : " "})
     })
     const res = await request.json()
     console.log(res)
     } catch (error) {
       console.error(`Couldn't retrieve ToDos: ${error.message}`)
     }
-
-    const newTodos = [todo, ...todos];
-
-    setTodos(newTodos);
+    getTodo()
     console.log(...todos);
   };
 
@@ -60,30 +61,70 @@ function TodoList() {
     setTodos(updatedTodos);
   };
 
-  const updateTodo = (todoId, newValue) => {
+  // Update al elemento todo
+  const updateTodo = async (todoId, newValue) => {
     if (!newValue.text || /^\s*$/.test(newValue.text)) {
       return;
     }
-
+    const { text, description } = newValue
+    try {
+      const request = await fetch(`https://todo-api-h8ov.onrender.com/api/${todoId}`, {
+      method: "PATCH",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({title: text, description: description.length>0 ? description : " "})
+    })
+    const res = await request.json()
+    console.log(res)
+    } catch (error) {
+      console.error(`Couldn't retrieve ToDos: ${error.message}`)
+    }
+    getTodo()
+    console.log(...todos);
+    /* console.log(newValue)
     setTodos((prev) =>
       prev.map((item) => (item.id === todoId ? newValue : item))
-    );
+    ); */
   };
 
-  const removeTodo = (id) => {
-    const removedArr = [...todos].filter((todo) => todo.id !== id);
-
-    setTodos(removedArr);
+  // Elimina elemento de la base de datos
+  const removeTodo = async (id) => {
+    try {
+      const request = await fetch(`https://todo-api-h8ov.onrender.com/api/${id}`, {
+      method: "DELETE"
+    })
+    const res = await request.json()
+    console.log(res)
+    } catch (error) {
+      console.error(`Couldn't retrieve ToDos: ${error.message}`)
+    }
+    getTodo()
   };
 
-  const completeTodo = (id) => {
-    let updatedTodos = todos.map((todo) => {
+  const completeTodo = async (id) => {
+    try {
+      const request = await fetch(`https://todo-api-h8ov.onrender.com/api/${id}`, {
+      method: "PATCH",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ isDone: false})
+    })
+    const res = await request.json()
+    console.log(res)
+    } catch (error) {
+      console.error(`Couldn't retrieve ToDos: ${error.message}`)
+    }
+    getTodo()
+    /* let updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
         todo.isComplete = !todo.isComplete;
       }
       return todo;
-    });
-    setTodos(updatedTodos);
+    }); */
   };
 
   return (
