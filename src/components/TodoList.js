f736ebf0cb8b89b4e09655e179e983772cbaf267
DeusNayo <deusnayo@gmail.com>
2023-07-12 15:51:44 -0600

    Project finished

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -15,9 +15,10 @@ function TodoList() {
     const res = await response.json()
     console.log(res)
     const newTodo = res.data.map((a) => {
-      return { id: a.id, text: a.title, description: a.description}
+      return { id: a.id, text: a.title, description: a.description, isComplete: Boolean(a.isDone)}
     })
     setTodos(newTodo)
+    console.log(todos)
     } catch (error) {
       console.error(`Couldn't retrieve ToDos: ${error.message}`)
     }
@@ -72,17 +73,13 @@ function TodoList() {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({title: text, description: description.length>0 ? description : " "})
+      body: JSON.stringify({title: text, description: description.length>0 ? description : " ", isDone:0})
     })
     console.log(await request.json())
     } catch (error) {
       console.error(`Couldn't retrieve ToDos: ${error.message}`)
     }
     getTodo()
-    /* console.log(newValue)
-    setTodos((prev) =>
-      prev.map((item) => (item.id === todoId ? newValue : item))
-    ); */
   };
 
   // Elimina elemento de la base de datos
@@ -100,38 +97,36 @@ function TodoList() {
 
   const completeTodo = async (id) => {
     try {
+      const todo = todos.filter((a) => a.id === id)
+      if(todo.length === 0) {
+        return;
+      }
       const request = await fetch(`http://localhost:4000/api/${id}`, {
       method: "PATCH",
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ isDone: false})
+      body: JSON.stringify({ title: todo[0].text, isDone: Number(!todo[0].isComplete), description: todo[0].description})
     })
     console.log(await request.json())
     } catch (error) {
       console.error(`Couldn't retrieve ToDos: ${error.message}`)
     }
     getTodo()
-    /* let updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.isComplete = !todo.isComplete;
-      }
-      return todo;
-    }); */
   };
 
   return (
     <>
       <h1>What's the Plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo
+      {todos.length>0 ? <Todo
         todos={todos}
         completeTodo={completeTodo}
         removeTodo={removeTodo}
         updateTodo={updateTodo}
         showDescription={showDescription}
-      />
+      /> : <h4 style={{color: "white"}}>There is nothing to do...</h4>}
     </>
   );
 }
